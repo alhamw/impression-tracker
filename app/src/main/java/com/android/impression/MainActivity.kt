217f420742eba76e0968be1eb510e53cd65e7bd4
alhamw <alham@styletheory.co>
2020-05-04 17:47:06 +0700

    sticky header

--- app/src/main/java/com/android/impression/MainActivity.kt
+++ app/src/main/java/com/android/impression/MainActivity.kt
@@ -3,14 +3,17 @@ package com.android.impression
 import android.graphics.Rect
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.item_browse.view.*
+import kotlinx.android.synthetic.main.item_browse.view.tv_position
+import kotlinx.android.synthetic.main.item_browse_header.view.*
 
 class MainActivity : AppCompatActivity() {
     val trackedData = mutableListOf<TimeTrackHolder>()
@@ -20,18 +23,29 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val products = mutableListOf<Product>()
-        for (i in 0..10) {
-            products.add(Product(i))
+        for (i in 0..20) {
+            products.add(Product(i, i == 2))
         }
 
-        rv_browse.adapter = BrowseAdapter(products)
-        rv_browse.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        val adapter = BrowseAdapter(products)
+        val layoutManager = GridLayoutManager(this, 2)
+        layoutManager.spanSizeLookup = object : GridLayoutManager.SpanSizeLookup() {
+            override fun getSpanSize(position: Int): Int {
+                return if (position == 2) 2 else 1
+            }
+        }
+
+        rv_browse.addItemDecoration(StickHeaderItemDecoration(adapter))
+        rv_browse.adapter = adapter
+        rv_browse.layoutManager = layoutManager
         rv_browse.smoothScrollBy(0, 5)
         rv_browse.addOnScrollListener(object : RecyclerView.OnScrollListener() {
             override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
                 super.onScrolled(recyclerView, dx, dy)
-                val firstPosition = (recyclerView.layoutManager as LinearLayoutManager).findFirstVisibleItemPosition()
-                val lastPosition = (recyclerView.layoutManager as LinearLayoutManager).findLastVisibleItemPosition()
+                val firstPosition =
+                    (recyclerView.layoutManager as LinearLayoutManager).findFirstVisibleItemPosition()
+                val lastPosition =
+                    (recyclerView.layoutManager as LinearLayoutManager).findLastVisibleItemPosition()
 
                 checkTrackedDataState(firstPosition, lastPosition)
 
@@ -44,8 +58,10 @@ class MainActivity : AppCompatActivity() {
                         val visibleHeight = itemRect.height()
                         val height = view.measuredHeight
 
-                        val visibleHeightPercent = visibleHeight.toDouble() / height.toDouble() * 100
-                        val position = (recyclerView.layoutManager as LinearLayoutManager).getPosition(view)
+                        val visibleHeightPercent =
+                            visibleHeight.toDouble() / height.toDouble() * 100
+                        val position =
+                            (recyclerView.layoutManager as LinearLayoutManager).getPosition(view)
 
                         trackViewHeightAndTime(visibleHeightPercent, position) {
                             Log.d("TAG", "$position")
@@ -82,15 +98,94 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    data class Product(val position: Int)
+    data class Product(val position: Int, val isHeader: Boolean = false)
 
-    class BrowseAdapter(val products: MutableList<Product>) :
+    class BrowseAdapter(val products: MutableList<Product>) : StickHeaderItemDecoration.StickyHeaderInterface,
+        RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+        override fun onCreateViewHolder(p0: ViewGroup, p1: Int): RecyclerView.ViewHolder {
+            return if (p1 == 1)
+                ItemViewHolder(
+                    LayoutInflater.from(p0.context).inflate(
+                        R.layout.item_browse_header,
+                        p0,
+                        false
+                    )
+                )
+            else
+                ItemViewHolder(
+                    LayoutInflater.from(p0.context).inflate(
+                        R.layout.item_browse,
+                        p0,
+                        false
+                    )
+                )
+        }
+
+        override fun getItemViewType(position: Int): Int {
+            return if (products[position].isHeader) 1 else 2
+        }
+
+        override fun getItemCount(): Int = products.size
+
+        override fun onBindViewHolder(p0: RecyclerView.ViewHolder, p1: Int) {
+            val itemViewHolder = p0 as ItemViewHolder
+            itemViewHolder.position = p1
+            itemViewHolder.setHeader()
+            itemViewHolder.setClick()
+        }
+
+        class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            fun setPosition(position: Int) {
+                itemView.tv_position.text = position.toString()
+            }
+
+            fun setHeader() {
+                itemView.rv_header?.let {
+                    it.adapter =
+                        BrowseHeaderAdapter(mutableListOf(Product(1, false), Product(1, false)))
+                    it.layoutManager = LinearLayoutManager(itemView.context, LinearLayoutManager.HORIZONTAL, false)
+                }
+            }
+
+            fun setClick() {
+                itemView.container?.setOnClickListener {
+                    Toast.makeText(it.context, "halo", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+        override fun isHeader(itemPosition: Int): Boolean {
+            return  products[itemPosition].isHeader
+        }
+
+        override fun getHeaderLayout(headerPosition: Int): Int {
+            return  if (products[headerPosition].isHeader)
+                R.layout.item_browse_header
+            else
+                R.layout.item_browse
+        }
+
+        override fun getHeaderPositionForItem(itemPosition: Int): Int {
+            return products.indexOfFirst { it.isHeader }
+        }
+
+        override fun bindHeaderData(header: View?, headerPosition: Int) {
+            header?.let {
+                val h = ItemViewHolder(header)
+                h.setHeader()
+                h.setClick()
+            }
+        }
+    }
+
+    class BrowseHeaderAdapter(val products: MutableList<Product>) :
         RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
         override fun onCreateViewHolder(p0: ViewGroup, p1: Int): RecyclerView.ViewHolder {
             return ItemViewHolder(
                 LayoutInflater.from(p0.context).inflate(
-                    R.layout.item_browse,
+                    R.layout.item_browse_header_item,
                     p0,
                     false
                 )
@@ -101,13 +196,10 @@ class MainActivity : AppCompatActivity() {
 
         override fun onBindViewHolder(p0: RecyclerView.ViewHolder, p1: Int) {
             val itemViewHolder = p0 as ItemViewHolder
-            itemViewHolder.position = p1
         }
 
         class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-            fun setPosition(position: Int) {
-                itemView.tv_position.text = position.toString()
-            }
+
         }
     }
 }
